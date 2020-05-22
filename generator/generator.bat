6009a57b339063c868f393f7ef8f0ef78bfe5916
Sewer <seweratwork@gmail.com>
2020-05-22 11:52:21 +0100

    Generate Windows DX9 and DX11 Bindings

--- generator/generator.bat
+++ generator/generator.bat
@@ -17,7 +17,7 @@ set PATH=%PATH%;C:\anima;C:\mingws\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\min
 :: arg[1] compiler name gcc, clang or cl
 :: arg[2] imgui_internal functions generation: true or false
 :: arg[3..n] name of implementations to generate
-luajit ./generator.lua gcc true glfw opengl3 opengl2 sdl
+luajit ./generator.lua gcc true win32 dx9 dx11
 
 ::leave console open
 cmd /k
